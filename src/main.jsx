c116a4fc06633251e1966e916469ff2a64863c77
Golden Diva <trandoanductai@gmail.com>
2025-04-01 16:56:50 +0700

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import "bootstrap/dist/css/bootstrap.min.css";
-import {Provider} from "react-redux";
-import {store} from "./store/store.js";
 import React from 'react';
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Provider store={store}>
+import ReactDOM from 'react-dom/client';
+import App from './App';
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-    </Provider>
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
